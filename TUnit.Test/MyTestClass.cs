37bec8820a76b45c7c82143463271f1e137aaea9
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2024-11-18 06:39:41 +1000

    Initial commit

---
+++ TUnit.Test/MyTestClass.cs
@@ -0,0 +1,22 @@
+namespace TUnit.Test;
+
+public class MyTestClass
+{
+    [Test]
+    [Arguments(1, 1, 2)]
+    [Arguments(1, 2, 3)]
+    [Arguments(2, 2, 4)]
+    [Arguments(4, 3, 7)]
+    [Arguments(5, 5, 10)]
+    public async Task MyTest(int value1, int value2, int expectedResult)
+    {
+        var result = Add(value1, value2);
+
+        await Assert.That(result).IsEqualTo(expectedResult);
+    }
+
+    private int Add(int x, int y)
+    {
+        return x + y;
+    }
+}
